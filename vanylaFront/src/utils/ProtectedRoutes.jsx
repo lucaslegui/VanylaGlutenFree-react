f2b9d405504016eb8467fe3df39d369a087ac204
Lucas Leguizamon <lucaslegui1107@gmail.com>
2024-07-03 17:15:38 -0300

    listo el ProtectedRoutes

---
+++ vanylaFront/src/utils/ProtectedRoutes.jsx
@@ -0,0 +1,12 @@
+import React, { useContext } from "react";
+import { AuthContext } from "../context/AuthContext.jsx";
+import { Navigate, Outlet } from "react-router-dom";
+
+const ProtectedRoutes = () => {
+    const { user } = useContext(AuthContext);
+    return (
+       user ? <Outlet/> : <Navigate to="/login"/>
+    );
+}
+
+export default ProtectedRoutes;
